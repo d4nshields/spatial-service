60eb313687e7b5ba0f904e73b0179f2fde3709c2
DanShieldsPala <daniel.shields@palainteractive.com>
2024-02-12 12:29:40 -0500

    removed https support for index page (breaks ajax calls on mac)

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import https from 'https';
-import fs from 'fs';
 
 dotenv.config();
 
@@ -24,7 +22,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/get_boundary_info', async (req, res) => {
-    const { lat, lon, clientapp } = req.query;
+    const { lat, lon} = req.query;
 
     if (!lat || !lon) {
         return res.status(400).json({ error: "Missing latitude or longitude parameter" });
@@ -75,14 +73,6 @@ app.get('/get_boundary_info', async (req, res) => {
 });
 
 // Read the SSL certificate files
-const privateKey = fs.readFileSync('./devkey.pem', 'utf8');
-const certificate = fs.readFileSync('./devcert.pem', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
-
-// Creating HTTPS server
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, '0.0.0.0', () => {
-    console.log(`Spatial service running over HTTPS on port ${port}`);
-});
+app.listen( port, () => {
+    console.log( `Spatial service running at http://localhost:${port}`);
+})
